8594b83fe6ddbbdcacdf83fb4bbf9c3ac906bad6
Will Howlett <wehowlett@gmail.com>
2018-06-16 15:59:44 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Starterkit</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default App
